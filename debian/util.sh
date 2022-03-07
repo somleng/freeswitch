b1177f729731bb31e4c8671d4c02879c294e7938
David Wilkie <dwilkie@gmail.com>
2022-03-07 13:17:25 +0700

    WIP

--- debian/util.sh
+++ debian/util.sh
@@ -98,13 +98,13 @@ getlib () {
 
 getlibs () {
   # get pinned libraries
-  getlib http://files.freeswitch.org/downloads/libs/sphinxbase-0.8.tar.gz
-  getlib http://files.freeswitch.org/downloads/libs/pocketsphinx-0.8.tar.gz
-  getlib http://files.freeswitch.org/downloads/libs/communicator_semi_6000_20080321.tar.gz
-  #getlib http://download.zeromq.org/zeromq-2.1.9.tar.gz \
-  #  || getlib http://download.zeromq.org/historic/zeromq-2.1.9.tar.gz
-  getlib http://files.freeswitch.org/downloads/libs/freeradius-client-1.1.7.tar.gz
-  #getlib http://files.freeswitch.org/downloads/libs/v8-3.24.14.tar.bz2
+  getlib https://github.com/somleng/freeswitch/raw/build_debian/libs/sphinxbase-0.8.tar.gz
+  getlib https://github.com/somleng/freeswitch/raw/build_debian/libs/pocketsphinx-0.8.tar.gz
+  getlib https://github.com/somleng/freeswitch/raw/build_debian/libs/communicator_semi_6000_20080321.tar.gz
+  #getlib https://download.zeromq.org/zeromq-2.1.9.tar.gz \
+  #  || getlib https://download.zeromq.org/historic/zeromq-2.1.9.tar.gz
+  getlib https://github.com/somleng/freeswitch/raw/build_debian/libs/freeradius-client-1.1.7.tar.gz
+  #getlib https://files.freeswitch.org/downloads/libs/v8-3.24.14.tar.bz2
 }
 
 check_repo_clean () {
